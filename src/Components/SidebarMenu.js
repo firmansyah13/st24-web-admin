faeccc79d22772ea5c898d61a5e7705db8838c14
firmansyah13 <ekalisni75@gmail.com>
2022-09-27 18:03:55 +0700

    web-admin

--- src/Components/SidebarMenu.js
+++ src/Components/SidebarMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Sidebar.css';
