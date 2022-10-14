b7caf1b36642a721afc84c7b12bbe858287d42dd
firmansyah13 <emank.tunk@gmail.com>
2022-10-14 03:05:51 +0000

    web-admin

--- src/Pages/Broadcast.js
+++ src/Pages/Broadcast.js
@@ -1,6 +1,18 @@
 import React from "react";
 import NavMenu from "../Components/NavMenu";
 import Sidebar from "../Components/SidebarMenu";
+import { Select2 } from "select2-react-component";
+// import { Select } from "flowbite-react";
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+];
+
+// const MyComponent = () => (
+//   <Select options={options} />
+// )
 
 const Broadcast = () => {
     return (
@@ -10,9 +22,17 @@ const Broadcast = () => {
                 <NavMenu />
                 <div className="container-fluid max-h-screen px-2">
                     <h1 className="text-2xl font-semibold text-left mt-2 mb-2">Broadcast</h1>
+                    <div className='flex max-h-screen'>
+                        <div className="rounded-xl border p-5 shadow-md h-full w-full bg-white">
+                            <Select2 data={options}
+                                value={this.value}
+                                update={value => this.update(value)}>
+                            </Select2>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default Broadcast;
